8bac0cc282a7051bfddb6278169e7cc7396ebb0e
jothishwar.S <84170801+Jothishwar@users.noreply.github.com>
2022-02-03 18:35:39 +0530

    Update script.js

--- CSS&JS clock (Day2)/script.js
+++ CSS&JS clock (Day2)/script.js
@@ -1,7 +1,14 @@
-const inputs = document.querySelectorAll('.options input');
-function Update() {
-	const suffix = this.dataset.sizing || '';
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+function setdate() {
+	const d=new Date();
+	const sec=d.getSeconds();
+	const min=d.getMinutes();
+	const hour=d.getHours();
+	const secDeg=((sec/60)*360)+90;
+	const minDeg=((min/60)*360)+((sec/60)*6)+90;
+	const hrDeg=((hour/12)*360)+((min/60)*30)+90;
+	document.querySelector('.second-hand').style.transform = `rotate(${secDeg}deg)`;
+	document.querySelector('.min-hand').style.transform = `rotate(${minDeg}deg)`;
+	document.querySelector('.hour-hand').style.transform = `rotate(${hrDeg}deg)`;
+	//console.log(hour,min,sec);
 }
-inputs.forEach(input => input.addEventListener('change', Update));
-inputs.forEach(input => input.addEventListener('mousemove', Update));
+setInterval(setdate,1000);
